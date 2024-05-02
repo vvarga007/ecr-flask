794eee0dea455d8ee924696179315b58401e9072
vvarga <viktor@varga.io>
2024-05-02 11:46:28 +0200

    Enable ECR tag overwrite

--- modules/ecr-flask/main.tf
+++ modules/ecr-flask/main.tf
@@ -1,7 +1,7 @@
 # tfsec:ignore:aws-ecr-repository-customer-key
 resource "aws_ecr_repository" "repository" {
   name                 = var.repository_name
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
   force_delete         = true
 
 
